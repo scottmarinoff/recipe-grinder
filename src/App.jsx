6f2b7d33c989ff4d167e0138313c4c0ad35bfc4a
Scott Marinoff <smarinoff@gmail.com>
2025-01-30 12:36:08 -0500

    Update .jsx files for React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import JSZip from "jszip";
 import { extractIngredients } from "./utils/ingredientParser";
 import { standardizeUnit } from "./utils/unitStandardizer";
